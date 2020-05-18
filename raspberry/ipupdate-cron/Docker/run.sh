8f32c1527b8ed3e9be89d5ce01a021c4ed157051
Sandro Felder <sandro.felder@swisscom.com>
2020-05-18 16:43:54 +0200

    update to root domain

--- raspberry/ipupdate-cron/Docker/run.sh
+++ raspberry/ipupdate-cron/Docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-ez-ipupdate -S zoneedit -u blacktr:$DYN_SECRET -h test.afterworkbeer.com
+ez-ipupdate -S zoneedit -u blacktr:$DYN_SECRET -h afterworkbeer.com
 echo "IP last updated at $timestamp"
