b81a2a9894f82b1e9bf0c90eaf28de45a1afe9ea
Sandro Felder <sandro.felder@swisscom.com>
2020-05-18 17:09:26 +0200

    change crone schedule

--- raspberry/ipupdate-cron/Docker/entrypoint.sh
+++ raspberry/ipupdate-cron/Docker/entrypoint.sh
@@ -8,7 +8,7 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-15 * * * * /run.sh >> /var/log/cron.log 2>&1
+*/11 * * * * /run.sh >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
