785c3b54d6a990b6bca28cc5f94d0b448f8584f5
Sandro Felder <sandro.felder@swisscom.com>
2020-05-19 10:29:29 +0200

    change run.sh to only update when necessary

--- raspberry/ipupdate-cron/Docker/run.sh
+++ raspberry/ipupdate-cron/Docker/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-ez-ipupdate -S zoneedit -u blacktr:$DYN_SECRET -h afterworkbeer.com
-echo "IP last updated at $timestamp"
+ip=`curl -s https://hostpapastatus.com/ip/ | grep -ioE "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
+oldip=$(cat ip.txt)
+if [[ $ip == $oldip ]]; then
+    echo "IP ($ip) not changed at $timestamp"
+else
+    echo $ip > ip.txt
+    ez-ipupdate -S zoneedit -u blacktr:$DYN_SECRET -h tt.afterworkbeer.com -a $ip
+    echo "IP ($ip) last updated at $timestamp"
+fi
